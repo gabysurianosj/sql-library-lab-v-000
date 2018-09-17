d5e11e35ad86edf14845b5873f215fdf1a79b0fa
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-17 14:05:56 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 1, 1); 
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 1, 1); 
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
+
+INSERT INTO subgenres (name) VALUES ("Fantasy");
+INSERT INTO subgenres (name) VALUES ("Magic");
+
+INSERT INTO authors (name) VALUES ("JRR Tolkien");
+INSERT INTO authors (name) VALUES ('JK Rowling');
