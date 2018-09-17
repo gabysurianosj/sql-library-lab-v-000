3ab7b3eb3af9d04c4808e86e64b413e4604de33d
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-17 14:34:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,23 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books
+    INNER JOIN series 
+      ON books.series_id = series.id 
+    WHERE series.id = 1 
+    ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters 
+    ORDER BY length(motto) DESC 
+    LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) AS num_species FROM characters
+    GROUP BY species
+    ORDER BY num_species DESC 
+    LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
