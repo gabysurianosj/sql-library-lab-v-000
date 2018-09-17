c8ccf0cc8c47101e8ce3d3724846ad2fda2ab89b
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-17 14:06:31 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,10 @@ INSERT INTO subgenres (name) VALUES ("Magic");
 INSERT INTO authors (name) VALUES ("JRR Tolkien");
 INSERT INTO authors (name) VALUES ('JK Rowling');
 
+INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Two Towers", 1954, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Return of the King", 1954, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Sorcerer's Stone", 1997, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Chamber of Secrets", 1998, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Goblet of Fire", 2000, 2);
+
